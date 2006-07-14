57b54438edf632ba44bf469772e4b6949ef33003
rolandas <rolandas@3231ed4b-2112-0410-84e0-8c3944a40730>
2006-07-14 13:07:05 +0000

    Branching by copying files from HEAD

---
+++ branches/razaIRC/shareaza/shareaza/LibraryHistory.h
@@ -0,0 +1,105 @@
+//
+// LibraryHistory.h
+//
+// Copyright (c) Shareaza Development Team, 2002-2005.
+// This file is part of SHAREAZA (www.shareaza.com)
+//
+// Shareaza is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2 of
+// the License, or (at your option) any later version.
+//
+// Shareaza is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Shareaza; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#if !defined(AFX_LIBRARYHISTORY_H__71D206EC_CDA8_46AD_9800_4A11C9EBAEA5__INCLUDED_)
+#define AFX_LIBRARYHISTORY_H__71D206EC_CDA8_46AD_9800_4A11C9EBAEA5__INCLUDED_
+
+#pragma once
+
+class CLibraryRecent;
+class CLibraryFile;
+
+
+class CLibraryHistory
+{
+// Construction
+public:
+	CLibraryHistory();
+	virtual ~CLibraryHistory();
+
+// Attributes
+protected:
+	CList< CLibraryRecent* > m_pList;
+
+public:
+	struct sTorrentDetails
+	{
+		CString			m_sName;
+		CString			m_sPath;
+		Hashes::BtHash  m_oBTH;
+		DWORD			m_tLastSeeded;
+		QWORD			m_nUploaded;
+		QWORD			m_nDownloaded;
+	};
+	
+	sTorrentDetails	LastSeededTorrent;			// Most recently seeded torrent (for home page button)
+	sTorrentDetails	LastCompletedTorrent;		// Most recently completed torrent that didn't reach 100% ratio
+
+
+// Operations
+public:
+	POSITION		GetIterator() const;
+	CLibraryRecent*	GetNext(POSITION& pos) const;
+	INT_PTR			GetCount() const { return m_pList.GetCount(); }
+	void			Clear();
+public:
+	BOOL			Check(CLibraryRecent* pRecent, int nScope = 0) const;
+	CLibraryRecent*	GetByPath(LPCTSTR pszPath) const;
+    CLibraryRecent*	Add(LPCTSTR pszPath, const Hashes::Sha1Hash& oSHA1, const Hashes::Ed2kHash& oED2K, LPCTSTR pszSources);
+	BOOL			Submit(CLibraryFile* pFile);
+	void			OnFileDelete(CLibraryFile* pFile);
+	void			ClearTodays();
+	int				Prune();
+	void			Serialize(CArchive& ar, int nVersion);
+
+};
+
+
+class CLibraryRecent
+{
+// Construction
+public:
+	CLibraryRecent();
+    CLibraryRecent(LPCTSTR pszPath, const Hashes::Sha1Hash& oSHA1, const Hashes::Ed2kHash& oED2K, LPCTSTR pszSources);
+	virtual ~CLibraryRecent();
+
+// Attributes
+public:
+	FILETIME		m_tAdded;
+	BOOL			m_bToday;
+public:
+	CLibraryFile*	m_pFile;
+public:
+    Hashes::Sha1Hash m_oSHA1;
+    Hashes::Ed2kHash m_oED2K;
+	CString			m_sPath;
+	CString			m_sSources;
+
+// Operations
+public:
+	void	RunVerify(CLibraryFile* pFile);
+	void	Serialize(CArchive& ar, int nVersion);
+
+};
+
+extern CLibraryHistory LibraryHistory;
+
+#endif // !defined(AFX_LIBRARYHISTORY_H__71D206EC_CDA8_46AD_9800_4A11C9EBAEA5__INCLUDED_)
