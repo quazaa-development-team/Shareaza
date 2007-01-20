7989aa3b8afc1a6a3f548421b7d934ab1dbb0d06
mylenejenius <mylenejenius@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-01-20 12:28:09 +0000

    Temp Pause only if DebugMode is set.

--- branches/Shareaza-2-2-2-x-CyberBob-Protocol-Develop-tests/shareaza/Download.cpp
+++ branches/Shareaza-2-2-2-x-CyberBob-Protocol-Develop-tests/shareaza/Download.cpp
@@ -663,7 +663,7 @@ BOOL CDownload::Load(LPCTSTR pszName)
 	m_bGotPreview = GetFileAttributes( m_sDiskName + L".png" ) != INVALID_FILE_ATTRIBUTES;
 	m_nSaveCookie = m_nCookie;
 	// only for debuging purpose
-	m_bTempPaused = TRUE;
+	m_bTempPaused = Settings.General.Debug;
 	
 	return bSuccess;
 }
