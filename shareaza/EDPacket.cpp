quazaa-development-team/Shareaza

commit 82911d94a7ebaf734bf0c4e136a1247d0a4658a5

Changed many "SOCKADDR_IN*" to "const SOCKADDR_IN*".
git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8741 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/EDPacket.cpp
@@ -316,8 +316,10 @@ CEDPacket* CEDPacket::ReadBuffer(CBuffer* pBuffer)
 	if ( pBuffer->m_nLength - sizeof(*pHeader) + 1 < pHeader->nLength ) return NULL;
 	CEDPacket* pPacket = CEDPacket::New( pHeader );
 	pBuffer->Remove( sizeof(*pHeader) + pHeader->nLength - 1 );
-	if ( pPacket->InflateOrRelease() ) return NULL;
-	return pPacket;
+	if ( pPacket->Inflate() )
+		return pPacket;	
+	pPacket->Release();
+	return NULL;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -326,7 +328,8 @@ CEDPacket* CEDPacket::ReadBuffer(CBuffer* pBuffer)
 BOOL CEDPacket::Deflate()
 {
 	if ( m_nEdProtocol != ED2K_PROTOCOL_EDONKEY &&
-		 m_nEdProtocol != ED2K_PROTOCOL_EMULE ) return FALSE;
+		 m_nEdProtocol != ED2K_PROTOCOL_EMULE )
+		 return FALSE;
 
 	DWORD nOutput = 0;
 	auto_array< BYTE > pOutput( CZLib::Compress( m_pBuffer, m_nLength, &nOutput ) );
@@ -339,57 +342,45 @@ BOOL CEDPacket::Deflate()
 
 	m_nEdProtocol = ED2K_PROTOCOL_EMULE_PACKED;
 
-	memcpy( m_pBuffer, pOutput.get(), nOutput );
+	delete [] m_pBuffer;
+	m_pBuffer = pOutput.get();
 	m_nLength = nOutput;
+	m_nBuffer = nOutput;
 
 	return TRUE;
 }
 
-BOOL CEDPacket::InflateOrRelease()
+BOOL CEDPacket::Inflate()
 {
 	if ( m_nEdProtocol != ED2K_PROTOCOL_EMULE_PACKED &&
 		 m_nEdProtocol != ED2K_PROTOCOL_KAD_PACKED &&
 		 m_nEdProtocol != ED2K_PROTOCOL_REVCONNECT_PACKED )
-		return FALSE;
+		return TRUE;
 
 	DWORD nOutput = 0;
 	auto_array< BYTE > pOutput( CZLib::Decompress( m_pBuffer, m_nLength, &nOutput ) );
-
-	if ( pOutput.get() != NULL )
-	{
-		switch ( m_nEdProtocol )
-		{
-		case ED2K_PROTOCOL_EMULE_PACKED:
-			m_nEdProtocol = ED2K_PROTOCOL_EMULE;
-			break;
-		case ED2K_PROTOCOL_KAD_PACKED:
-			m_nEdProtocol = ED2K_PROTOCOL_KAD;
-			break;
-		case ED2K_PROTOCOL_REVCONNECT_PACKED:
-			m_nEdProtocol = ED2K_PROTOCOL_REVCONNECT;
-			break;
-		}
-
-		if ( m_nBuffer >= nOutput )
-		{
-			CopyMemory( m_pBuffer, pOutput.get(), nOutput );
-			m_nLength = nOutput;
-		}
-		else
-		{
-			delete [] m_pBuffer;
-			m_pBuffer = pOutput.release();
-			m_nLength = nOutput;
-			m_nBuffer = nOutput;
-		}
-
+	if ( ! pOutput.get() )
 		return FALSE;
-	}
-	else
+
+	switch ( m_nEdProtocol )
 	{
-		Release();
-		return TRUE;
+	case ED2K_PROTOCOL_EMULE_PACKED:
+		m_nEdProtocol = ED2K_PROTOCOL_EMULE;
+		break;
+	case ED2K_PROTOCOL_KAD_PACKED:
+		m_nEdProtocol = ED2K_PROTOCOL_KAD;
+		break;
+	case ED2K_PROTOCOL_REVCONNECT_PACKED:
+		m_nEdProtocol = ED2K_PROTOCOL_REVCONNECT;
+		break;
 	}
+
+	delete [] m_pBuffer;
+	m_pBuffer = pOutput.release();
+	m_nLength = nOutput;
+	m_nBuffer = nOutput;
+
+	return TRUE;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -848,7 +839,7 @@ BOOL CEDTag::Read(CFile* pFile)
 	return TRUE;
 }
 
-BOOL CEDPacket::OnPacket(SOCKADDR_IN* pHost)
+BOOL CEDPacket::OnPacket(const SOCKADDR_IN* pHost)
 {
 	switch ( m_nEdProtocol )
 	{

shareaza/BENode.cpp
@@ -99,7 +99,7 @@ void CBENode::Clear()
 //////////////////////////////////////////////////////////////////////
 // CBENode add a child node
 
-CBENode* CBENode::Add(const LPBYTE pKey, size_t nKey)
+CBENode* CBENode::Add(LPCBYTE pKey, size_t nKey)
 {
 	switch ( m_nType )
 	{
@@ -426,7 +426,7 @@ CBENode* CBENode::Decode(const CBuffer* pBuffer, DWORD *pnReaden)
 	try
 	{
 		auto_ptr< CBENode > pNode( new CBENode() );
-		LPBYTE pInput	= pBuffer->m_pBuffer;
+		LPCBYTE pInput	= pBuffer->m_pBuffer;
 		DWORD nInput	= pBuffer->m_nLength;
 
 		if ( nInput > 1 && pInput[0] == '\r' && pInput[1] == '\n' )
@@ -451,7 +451,7 @@ CBENode* CBENode::Decode(const CBuffer* pBuffer, DWORD *pnReaden)
 	}
 }
 
-void CBENode::Decode(LPBYTE& pInput, DWORD& nInput, DWORD nSize)
+void CBENode::Decode(LPCBYTE& pInput, DWORD& nInput, DWORD nSize)
 {
 	ASSERT( m_nType == beNull );
 
@@ -475,10 +475,10 @@ void CBENode::Decode(LPBYTE& pInput, DWORD& nInput, DWORD nSize)
 
 		if ( nSeek >= 40 ) AfxThrowUserException();
 
-		pInput[nSeek] = 0;
-		if ( sscanf_s( (LPCSTR)pInput, "%I64i", &m_nValue ) != 1 )
+		char szFormat[ 8 ];
+		sprintf_s( szFormat, "%%%uI64u", nSeek );
+		if ( sscanf_s( (LPCSTR)pInput, szFormat, &m_nValue ) != 1 )
 			AfxThrowUserException();
-		pInput[nSeek] = 'e';
 
 		INC( nSeek + 1 );
 		m_nType = beInt;
@@ -515,7 +515,7 @@ void CBENode::Decode(LPBYTE& pInput, DWORD& nInput, DWORD nSize)
 
 			if ( nLen )
 			{
-				LPBYTE pKey = pInput;
+				LPCBYTE pKey = pInput;
 				INC( nLen );
 				Add( pKey, nLen )->Decode( pInput, nInput, nSize );
 			}
@@ -541,7 +541,7 @@ void CBENode::Decode(LPBYTE& pInput, DWORD& nInput, DWORD nSize)
 	m_nSize = nSize - nInput - m_nPosition;
 }
 
-int CBENode::DecodeLen(LPBYTE& pInput, DWORD& nInput)
+int CBENode::DecodeLen(LPCBYTE& pInput, DWORD& nInput)
 {
 	DWORD nSeek = 1;
 	for ( ; nSeek < 32 ; nSeek++ )
@@ -556,10 +556,10 @@ int CBENode::DecodeLen(LPBYTE& pInput, DWORD& nInput)
 		AfxThrowUserException();
 	int nLen = 0;
 
-	pInput[ nSeek ] = 0;
-	if ( sscanf_s( (LPCSTR)pInput, "%i", &nLen ) != 1 )
+	char szFormat[ 8 ];
+	sprintf_s( szFormat, "%%%ui", nSeek );
+	if ( sscanf_s( (LPCSTR)pInput, szFormat, &nLen ) != 1 )
 		AfxThrowUserException();
-	pInput[ nSeek ] = ':';
 	INC( nSeek + 1 );
 
 	if ( nInput < (DWORD)nLen )

shareaza/BENode.h
@@ -23,6 +23,7 @@
 
 class CBuffer;
 
+typedef const BYTE *LPCBYTE;
 
 class CBENode
 {
@@ -46,18 +47,18 @@ public:
 // Operations
 public:
 	void		Clear();
-	CBENode*	Add(const LPBYTE pKey, size_t nKey);
+	CBENode*	Add(LPCBYTE pKey, size_t nKey);
 	CBENode*	GetNode(LPCSTR pszKey) const;
 	CBENode*	GetNode(const LPBYTE pKey, int nKey) const;
 	CSHA		GetSHA1() const;
 	CString		GetStringFromSubNode(LPCSTR pszKey, UINT nEncoding, bool& pEncodingError) const;
 	CString		GetStringFromSubNode(int nItem, UINT nEncoding, bool& pEncodingError) const;
 	void		Encode(CBuffer* pBuffer) const;
-	void		Decode(LPBYTE& pInput, DWORD& nInput, DWORD nSize);
+	void		Decode(LPCBYTE& pInput, DWORD& nInput, DWORD nSize);
 	static CBENode*	Decode(const CBuffer* pBuffer, DWORD *pnReaden = NULL );
 
 private:
-	static int	DecodeLen(LPBYTE& pInput, DWORD& nInput);
+	static int	DecodeLen(LPCBYTE& pInput, DWORD& nInput);
 
 // Inline
 public:

shareaza/BTPacket.h
@@ -1,7 +1,7 @@
 //
 // BTPacket.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2009.
+// Copyright (c) Shareaza Development Team, 2002-2010.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -73,7 +73,7 @@ public:
 	}
 
 	// Packet handler
-	virtual BOOL OnPacket(SOCKADDR_IN* /*pHost*/) { return FALSE; } // Unused
+	virtual BOOL OnPacket(const SOCKADDR_IN* /*pHost*/) { return FALSE; } // Unused
 
 	friend class CBTPacket::CBTPacketPool;
 };

shareaza/CrawlSession.cpp
@@ -1,7 +1,7 @@
 //
 // CrawlSession.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2009.
+// Copyright (c) Shareaza Development Team, 2002-2010.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -154,7 +154,7 @@ void CCrawlSession::OnRun()
 //////////////////////////////////////////////////////////////////////
 // CCrawlSession process a crawl reply
 
-void CCrawlSession::OnCrawl(SOCKADDR_IN* pHost, CG2Packet* pPacket)
+void CCrawlSession::OnCrawl(const SOCKADDR_IN* pHost, CG2Packet* pPacket)
 {
 	if ( ! m_bActive ) return;
 
@@ -171,7 +171,7 @@ void CCrawlSession::OnCrawl(SOCKADDR_IN* pHost, CG2Packet* pPacket)
 //////////////////////////////////////////////////////////////////////
 // CCrawlSession find a crawled node
 
-CCrawlNode* CCrawlSession::Find(IN_ADDR* pAddress, BOOL bCreate)
+CCrawlNode* CCrawlSession::Find(const IN_ADDR* pAddress, BOOL bCreate)
 {
 	for ( POSITION pos = m_pNodes.GetTailPosition() ; pos ; )
 	{

shareaza/CrawlSession.h
@@ -1,7 +1,7 @@
 //
 // CrawlSession.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2007.
+// Copyright (c) Shareaza Development Team, 2002-2010.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -18,9 +18,6 @@
 // along with Shareaza; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-
-#if !defined(AFX_CRAWLSESSION_H__262E4F3E_1C28_4318_AE74_921A8392BFD9__INCLUDED_)
-#define AFX_CRAWLSESSION_H__262E4F3E_1C28_4318_AE74_921A8392BFD9__INCLUDED_
 
 #pragma once
 
@@ -49,9 +46,9 @@ public:
 	int			GetLeafCount();
 public:
 	void		OnRun();
-	void		OnCrawl(SOCKADDR_IN* pHost, CG2Packet* pPacket);
+	void		OnCrawl(const SOCKADDR_IN* pHost, CG2Packet* pPacket);
 protected:
-	CCrawlNode*	Find(IN_ADDR* pAddress, BOOL bCreate);
+	CCrawlNode*	Find(const IN_ADDR* pAddress, BOOL bCreate);
 
 	friend class CCrawlNode;
 };
@@ -91,8 +88,4 @@ protected:
 	enum { parseSelf, parseHub, parseLeaf };
 };
 
-
 extern CCrawlSession CrawlSession;
-
-
-#endif // !defined(AFX_CRAWLSESSION_H__262E4F3E_1C28_4318_AE74_921A8392BFD9__INCLUDED_)

shareaza/DCPacket.cpp
@@ -51,7 +51,7 @@ void CDCPacket::ToBuffer(CBuffer* pBuffer) const
 	pBuffer->Add( m_pBuffer, m_nLength );
 }
 
-BOOL CDCPacket::OnPacket(SOCKADDR_IN* pHost)
+BOOL CDCPacket::OnPacket(const SOCKADDR_IN* pHost)
 {
 	SmartDump( pHost, TRUE, FALSE );
 
@@ -69,7 +69,7 @@ BOOL CDCPacket::OnPacket(SOCKADDR_IN* pHost)
 	return FALSE;
 }
 
-BOOL CDCPacket::OnCommonHit(SOCKADDR_IN* /* pHost */)
+BOOL CDCPacket::OnCommonHit(const SOCKADDR_IN* /* pHost */)
 {
 	// Search result
 	// $SR Nick FileName<0x05>FileSize FreeSlots/TotalSlots<0x05>HubName (HubIP:HubPort)|

shareaza/DCPacket.h
@@ -47,9 +47,15 @@ protected:
 
 // Allocation
 public:
-	inline static CDCPacket* New()
+	inline static CDCPacket* New(const BYTE* pBuffer = NULL, DWORD nLength = 0)
 	{
-		return (CDCPacket*)POOL.New();
+		if ( CDCPacket* pPacket = (CDCPacket*)POOL.New() )
+		{
+			if ( pBuffer == NULL || pPacket->Write( pBuffer, nLength ) )
+				return pPacket;
+			pPacket->Release();
+		}
+		return NULL;
 	}
 
 	inline virtual void Delete()
@@ -58,10 +64,10 @@ public:
 	}
 
 	// Packet handler
-	virtual BOOL OnPacket(SOCKADDR_IN* pHost);
+	virtual BOOL OnPacket(const SOCKADDR_IN* pHost);
 
 protected:
-	BOOL OnCommonHit(SOCKADDR_IN* pHost);
+	BOOL OnCommonHit(const SOCKADDR_IN* pHost);
 
 	friend class CDCPacket::CDCPacketPool;
 };

commit 2a4cd0e22ae6aa4695a92860ba3557386d5fc026

Fixed ED2K compressed packet crash introduced in r8741.

shareaza/EDPacket.cpp
@@ -333,8 +333,7 @@ BOOL CEDPacket::Deflate()
 
 	DWORD nOutput = 0;
 	auto_array< BYTE > pOutput( CZLib::Compress( m_pBuffer, m_nLength, &nOutput ) );
-
-	if ( pOutput.get() == NULL )
+	if ( ! pOutput.get() )
 		return FALSE;
 
 	if ( nOutput >= m_nLength )
@@ -343,9 +342,10 @@ BOOL CEDPacket::Deflate()
 	m_nEdProtocol = ED2K_PROTOCOL_EMULE_PACKED;
 
 	delete [] m_pBuffer;
-	m_pBuffer = pOutput.get();
+	m_pBuffer = pOutput.release();
 	m_nLength = nOutput;
 	m_nBuffer = nOutput;
+	m_nPosition = 0;
 
 	return TRUE;
 }
@@ -379,6 +379,7 @@ BOOL CEDPacket::Inflate()
 	m_pBuffer = pOutput.release();
 	m_nLength = nOutput;
 	m_nBuffer = nOutput;
+	m_nPosition = 0;
 
 	return TRUE;
 }

shareaza/ZLib.cpp
@@ -85,8 +85,8 @@ BYTE* CZLib::Compress2(LPCVOID pInput, DWORD nInput, DWORD* pnOutput, DWORD nSug
 
 auto_array< BYTE > CZLib::Decompress(LPCVOID pInput, DWORD nInput, DWORD* pnOutput)
 {
-	// Guess how big the data will be decompressed, use nSuggest, or just guess it will be 6 times as big
-	for ( DWORD nSuggest = max( nInput * 6ul, 1024ul ); ; nSuggest *= 2 )
+	// Guess how big the data will be decompressed, use nSuggest, or just guess it will be 3 times as big
+	for ( DWORD nSuggest = nInput * 3; ; nSuggest *= 2 )
 	{
 		*pnOutput = nSuggest;
 
@@ -116,8 +116,8 @@ BYTE* CZLib::Decompress2(LPCVOID pInput, DWORD nInput, DWORD* pnOutput)
 {
 	BYTE* pBuffer = NULL;
 
-	// Guess how big the data will be decompressed, use nSuggest, or just guess it will be 6 times as big
-	for ( DWORD nSuggest = max( nInput * 6ul, 1024ul ); ; nSuggest *= 2 )
+	// Guess how big the data will be decompressed, use nSuggest, or just guess it will be 3 times as big
+	for ( DWORD nSuggest = nInput * 3; ; nSuggest *= 2 )
 	{
 		*pnOutput = nSuggest;
 